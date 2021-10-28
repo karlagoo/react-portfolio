4e08e40ec0a265e9b2615055569aa8addfc8ed0a
Karla Goo <karla_goo@yahoo.com>
2021-10-27 22:55:41 -0500

    imported bootstrap

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,60 +1,65 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Stack } from 'react-bootstrap';
 
 function NavTabs({ currentPage, handlePageChange }) {
   const linkStyle = { border: '1px black', padding: '5px' };
   return (
-    <nav className="main-header-menu">
-      <section
-        style={{
-          display: 'flex',
-          fontFamily: 'helvetica',
-          flexDirection: 'row',
-          alignItems: 'flex-start',
-          justifyContent: 'flex-end',
-          textDecoration: 'none',
-        }}>
-        <div style={linkStyle}>
-          <a
-            style = {{textDecoration:"none"}}
-            href="#aboutme"
-            onClick={() => handlePageChange('AboutMe')}
-            className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
-          >
-            About
+    <div>
+      <Stack direction="horizontal" gap={3}>
+        <div className="bg-light border">Karla Goo</div>
+        <div className="bg-light border ms-auto">
+          <div style={linkStyle}>
+            <a
+              style={{ textDecoration: "none" }}
+              href="#aboutme"
+              onClick={() => handlePageChange('AboutMe')}
+              className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
+            >
+              About
         </a>
+          </div>
         </div>
+
+        <div className="bg-light border">
         <div style={linkStyle}>
-          <a
-            style = {{textDecoration:"none"}}
-            href="#portfolio"
-            onClick={() => handlePageChange('Portfolio')}
-            className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-          >
-            Portfolio
+            <a
+              style={{ textDecoration: "none" }}
+              href="#portfolio"
+              onClick={() => handlePageChange('Portfolio')}
+              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+            >
+              Portfolio
         </a>
+          </div>
         </div>
+        <div className="bg-light border">
         <div style={linkStyle}>
-          <a
-            style = {{textDecoration:"none"}}
-            href="#contact"
-            onClick={() => handlePageChange('Contact')}
-            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-          >
-            Contact
+            <a
+              style={{ textDecoration: "none" }}
+              href="#contact"
+              onClick={() => handlePageChange('Contact')}
+              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+            >
+              Contact
         </a>
+          </div>
         </div>
+        <div className="bg-light border">
         <div style={linkStyle}>
-          <a
-            style = {{textDecoration:"none"}}
-            href="#resume"
-            onClick={() => handlePageChange('Resume')}
-            className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-          >
-            Resume
+            <a
+              style={{ textDecoration: "none" }}
+              href="#resume"
+              onClick={() => handlePageChange('Resume')}
+              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+            >
+              Resume
         </a>
+          </div>
         </div>
-      </section>
-    </nav>
+      </Stack>
+
+    </div>
   );
 }
 
