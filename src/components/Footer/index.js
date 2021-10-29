21ef50a115800dbac55498d14664555c0eabaa1a
Karla Goo <karla_goo@yahoo.com>
2021-10-28 23:51:17 -0500

    created footer and added icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
-// import "./style.css";
 
 export default function Footer() {
+    const icons = [
+        {
+            name: "fab fa-github",
+            link: "http://github.com/karlagoo"
+        },
+        {
+            name: "fab fa-linkedin",
+            link: "https://www.linkedin.com/in/karlagoo/"
+        },
+        {
+            name: "fab fa-twitter",
+            link: "https://twitter.com/itsgoocci"
+        }
+    ]
     return (
-        <footer>Foot</footer>
+        <section>
+            <footer className="container" style={{
+                position: "absolute",
+                width: "100%",
+                bottom: "0",
+                height:"2.5rem",
+                alignItems: "center",
+                backgroundColor: "whitesmoke"
+            }}>
+                <div className="Row">
+                {icons.map(icon =>
+                ( 
+                    <a href={icon.link} key={icon.name}><i> className={icon.name}</i></a>
+                )
+                )}
+                </div>
+            </footer>
+        </section>
     )
 
 }
--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -6,7 +6,7 @@ import karla from '../../assets/images/karla.jpeg'
 export default function AboutMe() {
     return (
         <>
-        <div className="Card" style={{maxWidth: "540px", display:"flex"}}>
+        {/* <div className="Card" style={{maxWidth: "540px", display:"flex"}}>
             <div className="Row no-gutters">
                 <div className="Column">
                     <img src={karla} style={{
@@ -21,9 +21,9 @@ export default function AboutMe() {
                     </div>
                 </div>
             </div>
-        </div>
+        </div> */}
 
-  {/* <Card style={{ 
+  <Card style={{ 
       display: 'flex',}}>
       <div>About Me!</div>
        <Card.Body>
@@ -37,7 +37,7 @@ export default function AboutMe() {
       I was born in Chicago and raised in Mexico. I studied marketing at the University of Illinois at Chicago. I am a history buff with an affinity for horror movies and webtoons. My love for web development started when I was a young kid trying to have the coolest MySpace page out of all my friends. As I grew older I knew I was meant to do something creative that would showcase my passion for all things weird! In an interesting turn of events, my passion for helping people turned me into working in the healthcare field after graduation, but something in me knew that I was meant to do something in web development. Currently I am a full-stack web development student at Northwestern University in my third week of a six-month course. As this course progresses, I am excited to build on to this portfolio and create works that will display my creativity!
       </Card.Text>
     </Card.Body>
-  </Card> */}
+  </Card>
   
 </>
     )
