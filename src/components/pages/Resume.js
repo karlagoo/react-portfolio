1309476b47f41a302085f152da599c1cc14e07d0
Karla Goo <karla_goo@yahoo.com>
2021-10-28 21:59:23 -0500

    Uploaded resume and created resume component

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
+import Pdf from "../../assets/images/KarlaGoo_resume.pdf"
 
 export default function Resume() {
     return (
-        <p>Resume!</p>
+        <section>
+            <div>
+                <div>
+                    <div>
+                    <h3>Front-End Proficiencies:</h3>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>jQuery</li>
+                        <li>React</li>
+                        <li>Bootsrap</li>
+                    </ul>
+                    <h3>Back-End Proficiencies:</h3>
+                    <ul>
+                        <li>API's</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>MySQL</li>
+                        <li>MongoDB, Mongoose</li>
+                        <li>MERN Stack</li>
+                    </ul>
+                    <p><a href={Pdf}>Download my Resume</a></p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
