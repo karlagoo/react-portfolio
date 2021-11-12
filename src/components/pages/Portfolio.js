3da997ae14b39d9edb557fd776cb67870eed5940
Karla Goo <karla_goo@yahoo.com>
2021-11-11 22:38:51 -0600

    Added project images and project index

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
+import Project from '../Projects';
+import Projects from '../Projects'
 
 export default function Portfolio() {
     const projects = useState([
         {
-            name: "password-generator",
+            name: "Password-Generator",
             link: "https://karlagoo.github.io/Password-Generator/"
         },
         {
-            name: "horoscope-memes",
+            name: "Star_Spangled_Memer",
             link: "https://p1t1-web-dev-wizards.github.io/Project1---horoscope-memes/"
         },
         {
-            name: "nanny-poppins",
+            name: "Nanny-Poppins",
             link: "https://nanny-poppins.herokuapp.com/"
         },
         {
-            name: "budet-tracker",
+            name: "Budget-Tracker",
             link: "https://budget-tracker-karlahw.herokuapp.com/",
         },
         {
-            name: "note-taker",
+            name: "Note-taker",
             link: "https://note-taker-karla-goo.herokuapp.com/"
         },
         {
@@ -29,6 +31,18 @@ export default function Portfolio() {
     ])
 
     return (
-        <p>Portfolio!</p>
+        <section>
+            <div>
+                <h1 className="text-center margin top">Projects</h1>
+                <div className="row no-gutters popup-gellery">
+                    {projects.map((project)=>{
+                        <Project 
+                            project={project}
+                            key={"project"}
+                        />
+                    })}
+                </div>
+            </div>
+        </section>
     )
 }
