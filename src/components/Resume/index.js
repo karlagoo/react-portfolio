70504a855f76e059d61150bbdac6839cafac212c
Karla Goo <karla_goo@yahoo.com>
2023-08-30 08:45:37 -0500

    updated resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers'
-import Pdf from "../../assets/images/Resume2023.pdf"
+import Pdf from "../../assets/images/karlagoo.pdf"
 
 export default function Resume() {
     const [pages] = useState([
